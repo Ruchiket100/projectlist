140a54f0c11cc14320f0b4d895cb847e3c51d45e
Ruchiket Borse <RuchiketBorse1@gmail.com>
2024-01-24 08:17:18 +0000

    sidebar created

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function SideBar (){
+    return(
+        <div className="w-full h-full  pl-6">
+            <img className="py-3 flex h-14" alt="projectlist-logo" src="https://dqy38fnwh4fqs.cloudfront.net/website/peerlist-logo-full.svg"/>
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    colors: {
-      accent:{
-        50: "#70D07E",
-        100: "#5ECB76",
-        200: "#4CC56D",
-        300: "#3BBF64",
-        400: "#29B95B",
-        500: "#00AA45",
-        600: "#009B3E",
-        700: "#008C37",
-        800: "#007D30",
-        900: "#006E29",
-      }
-    },
+    extend: {
+      colors:{
+        accent:{
+          50: "#70D07E",
+          100: "#5ECB76",
+          200: "#4CC56D",
+          300: "#3BBF64",
+          400: "#29B95B",
+          500: "#00AA45",
+          600: "#009B3E",
+          700: "#008C37",
+          800: "#007D30",
+          900: "#006E29",
+        }
+      },
       fontFamily: {
         sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
       },
-  }
-}
+    },
+  },
+  plugins: [],
+};
+export default config;
