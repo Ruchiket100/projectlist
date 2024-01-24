806d696df87a98807a462d8029bbbb8cd5c4225b
Ruchiket Borse <RuchiketBorse1@gmail.com>
2024-01-24 06:42:47 +0000

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      accent:{
+        50: "#70D07E",
+        100: "#5ECB76",
+        200: "#4CC56D",
+        300: "#3BBF64",
+        400: "#29B95B",
+        500: "#00AA45",
+        600: "#009B3E",
+        700: "#008C37",
+        800: "#007D30",
+        900: "#006E29",
+      }
     },
-  },
-  plugins: [],
-};
-export default config;
+      fontFamily: {
+        sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+  }
+}
